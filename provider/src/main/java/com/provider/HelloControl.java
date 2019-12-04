ba72da5f5c59ef2e463065ade0a6986f6fe3703a
zhouzhongyuan <zhouzhongyuan@SERVICECOMB.IO>
2019-12-04 11:51:58 +0800

    fix class name

--- provider/src/main/java/com/provider/HelloControl.java
+++ provider/src/main/java/com/provider/HelloControl.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class helloControl {
+public class HelloControl {
   @RequestMapping("/")
   public String providerHello() {
     return "Hello Spring-Boot-ServiceCenter !";
